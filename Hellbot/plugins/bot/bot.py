f5d60fb4c21434f19ad1b64601f1c11ac92e2d19
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2024-01-03 02:29:04 +0530

    Update bot.py

--- Hellbot/plugins/bot/bot.py
+++ Hellbot/plugins/bot/bot.py
@@ -1,5 +1,10 @@
+import heroku3
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, Message
+
+from Hellbot import HEROKU_APP
+from Hellbot.core import LOGS
+from Hellbot.functions.tools import restart
 
 from ..btnsG import gen_bot_help_buttons, start_button
 from . import HELP_MSG, START_MSG, BotHelp, Config, hellbot
@@ -27,10 +32,29 @@ async def help_pm(_, message: Message):
     )
 
 
+@hellbot.bot.on_message(filters.command("restart") & Config.AUTH_USERS)
+async def restart_clients(_, message: Message):
+    await message.reply_text("Restarted Bot Successfully ✅")
+    try:
+        if HEROKU_APP:
+            try:
+                heroku = heroku3.from_key(Config.HEROKU_APIKEY)
+                app = heroku.apps()[Config.HEROKU_APPNAME]
+                app.restart()
+            except:
+                await restart()
+        else:
+            await restart()
+    except Exception as e:
+        LOGS.error(e)
+
+
 BotHelp("Others").add(
     "start", "To start the bot and get the main menu."
 ).add(
     "help", "To get the help menu with all the command for this assistant bot."
+).add(
+    "restart", "To restart the bot."
 ).info(
     "Some basic commands of the bot."
 ).done()
