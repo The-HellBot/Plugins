1da5b078e5e8f7a0dec1474368d4407a59234588
Anand <72251918+HellBoy-OP@users.noreply.github.com>
2024-04-04 16:00:43 +0530

    Update afk.py

--- Hellbot/plugins/user/afk.py
+++ Hellbot/plugins/user/afk.py
@@ -61,7 +61,9 @@ async def afk(_, message: Message):
     reason = await hellbot.input(message)
     reason = reason if reason else "Not specified"
 
-    await db.set_afk(message.from_user.id, reason, media.id, media_type)
+    await db.set_afk(
+        message.from_user.id, reason, media.id if media else None, media_type
+    )
     await hellbot.delete(message, "🫡 𝖦𝗈𝗂𝗇𝗀 𝖠𝖥𝖪! 𝖲𝖾𝖾 𝗒𝖺'𝗅𝗅 𝗅𝖺𝗍𝖾𝗋.")
     await hellbot.check_and_log(
         "afk",
